88aee927681e7160c5b668035d99075cb50d669a
alcam92 <alancamargo92@yahoo.co.uk>
2017-03-07 21:17:34 -0300

    Implemented model package

--- app/src/main/java/com/ukdev/smartbuzz/model/Alarm.java
+++ app/src/main/java/com/ukdev/smartbuzz/model/Alarm.java
@@ -14,4 +14,88 @@ public class Alarm {
     private boolean sleepCheckerOn;
     private boolean active;
 
+    public Alarm() {
+        this(0, "", Calendar.getInstance(), 0, null, null, "", true);
+    }
+
+    public Alarm(int id, String title, Calendar triggerTime, int snoozeDuration, Day[] repetition,
+                 RingtoneWrapper ringtone, String text, boolean sleepCheckerOn) {
+        this.id = id;
+        this.title = title;
+        this.triggerTime = triggerTime;
+        this.snoozeDuration = snoozeDuration;
+        this.repetition = repetition;
+        this.ringtone = ringtone;
+        this.text = text;
+        this.sleepCheckerOn = sleepCheckerOn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Calendar getTriggerTime() {
+        return triggerTime;
+    }
+
+    public void setTriggerTime(Calendar triggerTime) {
+        this.triggerTime = triggerTime;
+    }
+
+    public int getSnoozeDuration() {
+        return snoozeDuration;
+    }
+
+    public void setSnoozeDuration(int snoozeDuration) {
+        this.snoozeDuration = snoozeDuration;
+    }
+
+    public Day[] getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(Day[] repetition) {
+        this.repetition = repetition;
+    }
+
+    public RingtoneWrapper getRingtone() {
+        return ringtone;
+    }
+
+    public void setRingtone(RingtoneWrapper ringtone) {
+        this.ringtone = ringtone;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isSleepCheckerOn() {
+        return sleepCheckerOn;
+    }
+
+    public void setSleepCheckerOn(boolean sleepCheckerOn) {
+        this.sleepCheckerOn = sleepCheckerOn;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
 }
--- app/src/main/java/com/ukdev/smartbuzz/model/RingtoneWrapper.java
+++ app/src/main/java/com/ukdev/smartbuzz/model/RingtoneWrapper.java
@@ -1,4 +1,23 @@
 package com.ukdev.smartbuzz.model;
 
+import android.net.Uri;
+
 public class RingtoneWrapper {
+
+    private String title;
+    private Uri uri;
+
+    public RingtoneWrapper(String title, Uri uri) {
+        this.title = title;
+        this.uri = uri;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Uri getUri() {
+        return uri;
+    }
+
 }
