569bc4fd6252118f8a50151aab4f8ad1f579d163
alcam92 <alancamargo92@yahoo.co.uk>
2017-03-13 20:22:52 -0300

    Implementing AlarmCreatorActivity

--- app/src/main/java/com/ukdev/smartbuzz/backend/AlarmHandler.java
+++ app/src/main/java/com/ukdev/smartbuzz/backend/AlarmHandler.java
@@ -9,6 +9,7 @@ import android.os.Build;
 import android.os.PowerManager;
 import android.provider.AlarmClock;
 import com.ukdev.smartbuzz.R;
+import com.ukdev.smartbuzz.activities.AlarmCreatorActivity;
 import com.ukdev.smartbuzz.backend.enums.Action;
 import com.ukdev.smartbuzz.backend.enums.Extra;
 import com.ukdev.smartbuzz.database.AlarmRepository;
@@ -106,7 +107,7 @@ public class AlarmHandler {
 
     public void setAlarmByVoice(Context context, Intent intent) {
         if (!intent.hasExtra(AlarmClock.EXTRA_HOUR)) {
-            Intent i = new Intent(context, null); // TODO: replace null with AlarmCreatorActivity.class
+            Intent i = new Intent(context, AlarmCreatorActivity.class);
             i.setAction(Action.CREATE_ALARM.toString());
             i.putExtra(Extra.SLEEP_CHECKER_ON.toString(), true);
             context.startActivity(i);
---
+++ app/src/main/java/com/ukdev/smartbuzz/frontend/CustomTimePicker.java
@@ -0,0 +1,33 @@
+package com.ukdev.smartbuzz.frontend;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewParent;
+import android.widget.TimePicker;
+
+public class CustomTimePicker extends TimePicker {
+
+    public CustomTimePicker(Context context) {
+        super(context);
+    }
+
+    public CustomTimePicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomTimePicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (ev.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            ViewParent p = getParent();
+            if (p != null)
+                p.requestDisallowInterceptTouchEvent(true);
+        }
+        return false;
+    }
+
+}
