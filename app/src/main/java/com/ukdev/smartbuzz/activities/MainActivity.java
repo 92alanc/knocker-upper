d5462a40e673221fe58487ec9f21bf3303250bf3
alcam92 <alancamargo92@yahoo.co.uk>
2017-03-14 22:08:53 -0300

    Implemented app info (menu)

--- app/src/main/java/com/ukdev/smartbuzz/activities/MainActivity.java
+++ app/src/main/java/com/ukdev/smartbuzz/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ukdev.smartbuzz.activities;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import com.ukdev.smartbuzz.R;
+import com.ukdev.smartbuzz.misc.LogTool;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,9 +34,25 @@ public class MainActivity extends AppCompatActivity {
             case R.id.menuItem_Settings:
                 break;
             case R.id.menuItem_About:
+                showInfo();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showInfo() {
+        try {
+            PackageManager manager = getPackageManager();
+            PackageInfo info = manager.getPackageInfo(getPackageName(), 0);
+            String version = info.versionName;
+            String appName = getString(R.string.app_name);
+            String about = getString(R.string.about);
+            String text = String.format("%1$s %2$s\n%3$s", appName, version, about);
+            LogTool log = new LogTool(this);
+            log.info(text);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- app/src/main/java/com/ukdev/smartbuzz/misc/LogTool.java
+++ app/src/main/java/com/ukdev/smartbuzz/misc/LogTool.java
@@ -2,14 +2,17 @@ package com.ukdev.smartbuzz.misc;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.StringRes;
 import android.support.v7.app.AlertDialog;
 import com.ukdev.smartbuzz.R;
 
 public class LogTool {
 
     private AlertDialog.Builder dialogue;
+    private Context context;
 
     public LogTool(Context context) {
+        this.context = context;
         dialogue = new AlertDialog.Builder(context);
         dialogue.setTitle(R.string.app_name);
     }
@@ -28,7 +31,7 @@ public class LogTool {
 
     public void info(String text) {
         dialogue.setMessage(text);
-        dialogue.setIcon(R.mipmap.ic_priority_high_white_24dp);
+        dialogue.setIcon(R.drawable.app_icon);
         dialogue.setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -36,6 +39,10 @@ public class LogTool {
             }
         });
         dialogue.show();
+    }
+
+    public void info(@StringRes int resId) {
+        info(context.getString(resId));
     }
 
 }
