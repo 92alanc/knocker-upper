6ffa615cbe9a518829fab8c9aa24eabd06d6413d
C5255008 <alan.camargo@sap.com>
2017-06-28 10:59:29 -0300

    Implementing fragments#

---
+++ app/src/main/java/com/ukdev/smartbuzz/fragments/RepetitionFragment.java
@@ -0,0 +1,58 @@
+package com.ukdev.smartbuzz.fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ukdev.smartbuzz.R;
+
+/**
+ * Fragment containing repetition information
+ *
+ * @author Alan Camargo
+ */
+public class RepetitionFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final boolean attachToRoot = false;
+        return inflater.inflate(R.layout.fragment_repetition, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                                               + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
